7d056c07478c8a14756de319aa7beb5620e7259b
lithiumgit <tulshi.chandra2.das@gmail.com>
2024-09-08 16:32:51 +0600

    set up next js

--- src/home.ts
+++
@@ -1,3 +0,0 @@
-import { age,T3 } from './modules/test.js';
-T3("a");
-console.log("age",age);
--- src/modules/test.ts
+++
@@ -1,8 +0,0 @@
-function T3(p:string){
-    console.log("ok done",p);
-}
-
-const name = "Jesse";
-const age = 40;
-
-export {name, age,T3};
