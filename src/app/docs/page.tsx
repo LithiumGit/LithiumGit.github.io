439ee5c17a45da3cadc3cccad92524a4047a3faf
lithiumgit <tulshi.chandra2.das@gmail.com>
2024-09-26 01:33:25 +0600

    hide expand icon on expanded

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -21,18 +21,18 @@ export default function Docs(){
     const [state,setState] = useMultiState<IState>({menuExpaned:false});
 
     return <main className="docs d-flex w-100 overflow-auto">
-        <div className={`menus overflow-auto ps-1 position-absolute ${state.menuExpaned?"d-block":""}`} 
+        <div className={`menus overflow-auto ps-1 position-absolute bg-color ${state.menuExpaned?"d-block":""}`} 
             style={{top:Constants.navHeight,left:0 , height:`calc(100% - ${Constants.navHeight} - ${Constants.footerHeight})`
             ,zIndex:ZIndexes.DocsNav}} >
             {DocumentMenus.list.map(m=>(
                 <SingleMenu key={m.name} menu={m} />
             ))}
         </div>
-        <span className="menus-expand position-absolute h4 text-primary" onClick={()=> setState({menuExpaned:true})}
+        {!state.menuExpaned && <span className="menus-expand position-absolute h4 text-primary" onClick={()=> setState({menuExpaned:true})}
         style={{top:Constants.navHeight,left:0
             ,zIndex:ZIndexes.DocsNav}} >
                 &gt;
-            </span>
+            </span>}
         <div className={`menus-placeholder ${state.menuExpaned?"d-block":""}`} />
         <div className="content h-100 overflow-auto ps-3 border-start">
             <GetStarted />
