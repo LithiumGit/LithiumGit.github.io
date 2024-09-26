30855d1ee86f8879ee7780c2a58e44caa9475bb8
lithiumgit <tulshi.chandra2.das@gmail.com>
2024-09-27 02:09:53 +0600

    make download page responsive

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -55,8 +55,8 @@ export default function Download(){
     }
     
 
-    return <main>
-        <div className='d-flex align-items-center justify-content-center'>
+    return <main className='w-100 download'>
+        <div className='w-100 d-flex align-items-center justify-content-center'>
             <Dropdown className=''>
                 <DropdownToggle variant="" id="" className="rounded-0 d-flex align-items-center">
                     {getOsIcon()}
@@ -88,28 +88,32 @@ export default function Download(){
             </a>                
         </div>
         <br />
-        <div className='ps-3'>
-            <h4>Installation Steps</h4>
-            <div className='ps-5'>
-                <h5>-Download the installer</h5>
-                <h5>-Execute the installer after download</h5>
-                <div className='pt-3'>
-                    <img src={inst1.src} alt="step1" />
-                    <h5>-Accept the agreement</h5>
+        <div className='row g-0 pt-5'>
+            <div className='col-md-3' />
+            <div className='col-12 col-md-6 ps-2'>
+                <h4>Installation Steps</h4>
+                <div className=''>
+                    <h5>-Download the installer</h5>
+                    <h5>-Execute the installer after download</h5>
+                    <div className='pt-3'>
+                        <img className='step-img' src={inst1.src} alt="step1" />
+                        <h5>-Accept the agreement</h5>
+                    </div>
+                    <div className='pt-3'>
+                        <img className='step-img' src={inst2.src} alt="step2" />
+                        <h5>-Choose the user. The default selected option(Only for me) is recommended.</h5>
+                    </div>
+                    <div className='pt-3'>
+                        <img className='step-img' src={inst3.src} alt="step2" />
+                        <h5>-Select the destination folder, click install and wait some for a moment.</h5>
+                    </div><div className='pt-3'>
+                        <img className='step-img' src={inst5.src} alt="step2" />
+                        <h5>-Click Finish. Installation completed.</h5>
+                    </div>
+                    <p className='pt-3'>NB:*Antivirus may prevent the download and installation process as the code signature not yet added to LithiumGit. In this case you have to download and install it forcely.</p>
                 </div>
-                <div className='pt-3'>
-                    <img src={inst2.src} alt="step2" />
-                    <h5>-Choose the user. The default selected option(Only for me) is recommended.</h5>
-                </div>
-                <div className='pt-3'>
-                    <img src={inst3.src} alt="step2" />
-                    <h5>-Select the destination folder, click install and wait some for a moment.</h5>
-                </div><div className='pt-3'>
-                    <img src={inst5.src} alt="step2" />
-                    <h5>-Click Finish. Installation completed.</h5>
-                </div>
-                <p className='pt-3'>NB:*Antivirus may prevent the download and installation process as the code signature not yet added to LithiumGit. In this case you have to download and install it forcely.</p>
             </div>
+            <div className='col-md-3' />
         </div>
     </main>
 }
