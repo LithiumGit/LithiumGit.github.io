e0c544c2e5acf88f1c4b3192661ee6307392daa0
lithiumgit <tulshi.chandra2.das@gmail.com>
2024-09-29 12:45:03 +0600

    fix footer ui issue

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ import { Constants, Routes } from "../../lib";
 
 export function Footer(){
     return (
-        <footer style={{height:Constants.footerHeight}} className="d-flex align-items-center justify-content-center text-nowrap overflow-auto text-slight bg-brand py-1">
+        <footer style={{height:Constants.footerHeight}} className="w-100 align-items-center justify-content-center text-nowrap overflow-auto text-slight bg-brand py-1">
             <span className="px-2">&copy;2021-Present LithiumGit Contributors.</span>
             <a href={Routes.Privacy} className="underline px-2 hover">Privacy Policy</a>
             <a href={Routes.Terms} className="underline px-2 hover">Terms of Use</a>
