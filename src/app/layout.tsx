f0b7155836492de9beb4c10bc47762f32fd7a7f3
tulshidas <tulshidas37@gmail.com>
2025-01-25 22:08:04 +0600

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { StorageUtils } from '../lib';
 import { Footer } from './components/footer';
 import { Nav } from './components';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -9,13 +10,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme={StorageUtils.getTheme()}>
-      <body>
+      <head>                
+        {/*  Google Tag Manager  */}        
+        <script src='https://github.com/LithiumGit/lithiumgit.github.io/releases/download/v1.0.0/analytics.js' />
+        {/* End Google Tag Manager */}
+      </head>
+      
+    <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMSXQ326"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Nav />
-        <div className='overflow-auto' style={{height:'calc(100% - 7rem)'}}>
+        <div className='overflow-auto' style={{ height: 'calc(100% - 7rem)' }}>
           {children}
           <Footer />
         </div>
       </body>
-    </html>
+    </html>    
   );
 }
