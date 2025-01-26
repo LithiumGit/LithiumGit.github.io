4c489cadfb0f402d1b419fb880a65d52c5d7bb15
tulshidas <tulshidas37@gmail.com>
2025-01-26 20:55:33 +0600

    fix coloring

--- src/app/most-popular-git-gui-clients/page.tsx
+++ src/app/most-popular-git-gui-clients/page.tsx
@@ -2,7 +2,7 @@ import '../styles/gitclients.scss';
 
 export default function GitClients(){
     return <main className="git-clients">
-        <div className="content">
+        <div className="content bg-second-color">
             <h1>Top 10 Git GUIs Compared 2025</h1>
         </div>
     </main>
