37fb5bce43033221e83560ddb5438923e5eb63c9
tulshidas <tulshidas37@gmail.com>
2025-01-27 20:55:17 +0600

    fix year

--- src/app/most-popular-git-gui-clients/page.tsx
+++ src/app/most-popular-git-gui-clients/page.tsx
@@ -7,7 +7,7 @@ export default function GitClients(){
     return <main className="git-clients">
         <div className="content bg-second-color">
             <h1>Top Git GUI Clients Compared 2025</h1>
-            <p>Explore the top Git GUI tools of 2024, each tailored for various
+            <p>Explore the top Git GUI tools of 2025, each tailored for various
                 developer needs. From seamless integrations to intuitive interfaces,
                 find the perfect Git client for your workflow.
             </p>
